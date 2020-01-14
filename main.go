61b4e822db8323bebe2295247860075a435182e1
Jack Dockerty <jdockerty19@gmail.com>
2020-01-14 13:39:26 +0000

    Appending tasks to JSON file, added example tasks into MyTasks.json

--- main.go
+++ main.go
@@ -7,61 +7,126 @@ import (
 	"io/ioutil"
 	"os"
 	"runtime"
+	"strconv"
 	"strings"
 )
 
-type Tasks struct {
-	TaskName    string 
-	TaskSummary string 
-	CompletedBy  string
+type Task struct {
+	TaskName    string
+	TaskSummary string
+	CompletedBy string
 }
 
-func optionOne() {
-	fmt.Println("Option one selected.")
+type Tasks []Task
+
+func addNewTasks() {
+	fmt.Println("How many tasks would you like to add?")
+	fmt.Print("Enter a value: ")
+	taskCount, err := strconv.Atoi(readUserInput())
+	if err != nil {
+		fmt.Println("String conversion error:", err)
+	}
+	fmt.Printf("Creating %d tasks...\n", taskCount)
+	var myNewTasks Tasks
+	for i := 1; i <= taskCount; i++ {
+		fmt.Print("Enter a task name: ")
+		taskNameIn := readUserInput()
+
+		fmt.Print("Enter a summary of the task: ")
+		taskSummaryIn := readUserInput()
+
+		fmt.Print("Enter the date that the task should be completed by: ")
+		completedByIn := readUserInput()
+
+		newTask := Task{taskNameIn, taskSummaryIn, completedByIn}
+		myNewTasks = append(myNewTasks, newTask)
+	}
+	fmt.Println("Tasks added:", jsonFormatToString(myNewTasks))
+	writeToJSONFile(myNewTasks)
+
 }
 
-func printJSON(i interface{}) string {
-	s, _ := json.MarshalIndent(i, "", "\t")
-	return string(s) + "\n"
+func jsonFileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
 }
 
-func optionTwo() {
+func writeToJSONFile(taskList Tasks) {
+	var newTask Tasks
+	jsonFile, err := os.OpenFile("MyTasks.json", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644)
+	if err != nil {
+		fmt.Println("Error opening:", err)
+		exitProgram()
+	}
+	defer jsonFile.Close()
+	bytes, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Println("Error reading:", err)
+		exitProgram()
+	}
+	err = json.Unmarshal(bytes, &newTask)
+	for _, task := range taskList {
+		newTask = append(newTask, task)
+	}
+	taskJSON, _ := json.Marshal(newTask)
+	err = ioutil.WriteFile("MyTasks.json", taskJSON, 0644)
+	fmt.Println("Written to file...")
+}
+
+func jsonFormatToString(i interface{}) string {
+	jsonData, _ := json.MarshalIndent(i, "", "\t")
+	return string(jsonData) + "\n"
+}
+
+
+func viewAllTasks() {
 	var myTasks Tasks
 
 	jsonTasks, err := os.Open("MyTasks.json")
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
-	fmt.Println("JSON file opened...")
-
+	fmt.Printf("JSON file: '%s' opened\n", jsonTasks.Name())
+	fmt.Println("All tasks...")
 	defer jsonTasks.Close()
 
 	byteVals, _ := ioutil.ReadAll(jsonTasks)
 
 	json.Unmarshal(byteVals, &myTasks)
-	fmt.Println(printJSON(myTasks))	
-	}
+	fmt.Println(jsonFormatToString(myTasks))
+}
 
+func readUserInput() string {
+	reader := bufio.NewReader(os.Stdin)
+	userInput, _ := reader.ReadString('\n')
+	if runtime.GOOS == "windows" {
+		userInput = strings.TrimSuffix(userInput, "\r\n")
+	} else {
+		userInput = strings.TrimSuffix(userInput, "\n")
+	}
+	return userInput
+}
 
 func taskMenu() {
 	fmt.Println("1 - Add new task.\n2 - View current tasks.")
-	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Print("Select an option value: ")
-		userInput, _ := reader.ReadString('\n')
-		if runtime.GOOS == "windows" {
-			userInput = strings.TrimSuffix(userInput, "\r\n")
-		} else {
-			userInput = strings.TrimSuffix(userInput, "\n")
-		}
-
-		switch userInput {
+		switch readUserInput() {
 		case "1":
-			optionOne()
+			addNewTasks()
 		case "2":
-			optionTwo()
+			viewAllTasks()
+		case "exit":
+			exitProgram()
 		}
 	}
+}
+
+func exitProgram() {
+	os.Exit(3)
 }
 
 func main() {
